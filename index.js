3537bde172c6972ee62cdf1a87b3a56053f869a4
Yizhong Wang <wang.6689@osu.edu>
2023-03-16 21:05:55 -0400

    require intern,managerm employee and engineer clas

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const inquirer = require("inquirer")
-
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+const Employee = require("./lib/Employee");
 const employees = [];
 
 inquirer
